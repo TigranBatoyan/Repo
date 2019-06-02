5da7b15b5d383710a69df1ca11c2f6385e7e68d8
TigranBatoyan <51286422+TigranBatoyan@users.noreply.github.com>
2019-06-03 00:48:00 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-alert('Hello')
+alert(menu.innerHTML)
     var li=document.getElementsByTagName('li')
     for(var n=0;n<li.length;n++){
-        
-            alert(555)
-    
+        li[n].addEventListener('click',function(){
+            alert(this.innerHTML)
+        })
     }
